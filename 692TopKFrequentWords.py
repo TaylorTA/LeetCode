fa641bc24dd2290517873da38f40be7fc21818b8
Taylor1001001 <tayloryuyiding@gmail.com>
2022-10-19 17:13:20 -0400

    Update 692TopKFrequentWords.py

--- 692TopKFrequentWords.py
+++ 692TopKFrequentWords.py
@@ -3,3 +3,12 @@ class Solution:
         count = Counter(words)
         topn = nsmallest(k, count.keys(), key = lambda x:(-count[x],x))
         return topn
+
+from sortedcontainers import SortedList    
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        c = Counter(words)
+        sl = SortedList(key=lambda pair: (-pair[0], pair[1]))
+        for w in c:
+            sl.add((c[w], w))
+        return [sl[i][1] for i in range(k)]
